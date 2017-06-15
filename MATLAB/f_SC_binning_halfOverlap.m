dadbe224759d91201fa2d4734a4428c09ed4b09d
biomemsLAB <manuel.ciba@h-ab.de>
2017-06-15 03:17:18 +0200

    Add files via upload

---
+++ MATLAB/f_SC_binning_halfOverlap.m
@@ -0,0 +1,31 @@
+%% bin a sequence of events (e.g. time stamps of spikes) with a bin overlap of 1/2*binSize
+% Input:    y: signal (e.g. time stamps)
+%           time_start:         time where first bin starts (in seconds)
+%           time_end:           time where last bin ends (in seconds)
+%           bin: binsize in seconds to discretize spiketrain
+%           flag_binary=1: frequencies can be only 0 or 1, flag_binary=0: normal mode
+% Output:   y_binned:       binned signal
+%           x_step:         corresponding x values (=bin center) of y_binned
+%           edges_step:     edges of bins
+%
+% written by Manuel Ciba, 2016/2017
+
+function [y_binned,x_step,edges_step]=f_SC_binning_halfOverlap(y,time_start,time_end,binsize,flag_binary) 
+
+    %% 1) new overlap binning method (faster)
+    step=binsize/2;
+    edges_step=time_start:step:time_end; % rec_dur + step: edges(end)>=rec_dur
+    x_step=edges_step(1:end-1)+step;
+    y_binned = histcounts(y,edges_step);
+    
+    %y_binned(1:end-1)= y_binned(1:end-1) + y_binned(2:end); % old
+    
+    y_binned_temp = [y_binned, 0];
+    y_binned(1:end)= y_binned(1:end) + y_binned_temp(2:end);
+       
+    %% 3) if flag_binary is true set all values >0 to 1
+    if flag_binary
+       y_binned(y_binned>0)=1; 
+    end
+
+end
